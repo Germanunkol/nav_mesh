fdbe6a8450f65f7e97d9f1aa08696a70b9a4a234
Germanunkol <germanunkol@germanunkol.de>
2022-11-26 17:30:35 +0100

    Added missing loader file

---
+++ nav_mesh/loader.py
@@ -0,0 +1,24 @@
+import pickle
+
+class RenameUnpickler(pickle.Unpickler):
+
+    def __init__( self, filename, old_module_name, new_module_name ):
+
+        self.old_module_name = old_module_name
+        self.new_module_name = new_module_name
+
+        pickle.Unpickler.__init__( self, filename )
+
+    def find_class(self, module, name):
+
+        if self.old_module_name in module:
+            print("Found old module name in:", module )
+            module = module.replace( self.old_module_name, self.new_module_name )
+            print("Replaced by:", module )
+
+        return pickle.Unpickler.find_class( self, module, name )
+
+def renamed_load(file_obj, old_module_name, new_module_name ):
+    return RenameUnpickler(file_obj,
+            old_module_name,
+            new_module_name).load()
